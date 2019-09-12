43114d83619f67bded8be0cb47e6b71bca4364a9
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-09-12 11:25:33 -0400

    Quiz 2

---
+++ app.js
@@ -0,0 +1,12 @@
+let teamJohn = (100 + 100 + 100) / 3;
+let teamMike = (100 + 100 + 100) / 3;
+
+if (teamJohn > teamMike) {
+    console.log(`John has a higher average of ${teamJohn} meaning he is the winner!`);
+}
+else if (teamMike > teamJohn){
+    console.log(`Mike has a higher average of ${teamMike} meaning he is the winner!`);
+} 
+else{
+    console.log(`Sadly, it's a tie.`)
+}
